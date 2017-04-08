9883a320cce25bd7ccd686deebc7061b148493fe
qing guan <740493643@qq.com>
2017-04-08 21:48:18 +0800

    170408

--- hardware/car/car.h
+++ hardware/car/car.h
@@ -1,7 +1,20 @@
 #ifndef _CAR_H
 #define _CAR_H
 
+#include "sys.h"
+#include "ycjobject.h"
 
+//定义函数指针
+typedef u8 (*carid) (void);
+
+//车类
+struct car
+{
+	Const
+		carid ID;					//类的类型标识
+	Variable
+		
+};
 
 
 #endif
